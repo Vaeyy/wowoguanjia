4c1a1188d6ddc1ddc5b5aa3ff2f51f0fb1a69b1b
Vae <523003115@qq.com>
2020-08-11 15:57:02 +0800

    修改了一些信息

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vae
+ * Date: 2020/8/11
+ * Time: 14:12
+ */
+
+namespace app\admin\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+        //后台登陆
+    public  function  login()
+    {   
+         return   view();
+    }
+
+}
